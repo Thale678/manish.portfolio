59e308797a688e0d63f6cc47d0fd5c061b035a06
Thale678 <manishthale69@gmail.com>
2025-08-01 10:33:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // This is the crucial line for GitHub Pages
-  base: '/MyPortfolio/', 
+  base: '/manish.portfolio/',
   plugins: [react()],
 })
